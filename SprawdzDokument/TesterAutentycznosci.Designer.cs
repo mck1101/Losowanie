ac2ea5ad85f13d159a07300db9bc0eaa85000755
mck1101 <matusik.maciej@wp.pl>
2019-01-10 13:10:11 +0100

    Importuj projekt

---
+++ SprawdzDokument/TesterAutentycznosci.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace SprawdzDokument
+{
+    partial class TesterAutentycznosci
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TesterAutentycznosci));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.wynik = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button7 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "50%";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(139, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "60%";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(220, 103);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "70%";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(301, 103);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "80%";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(382, 103);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "90%";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(463, 103);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "95%";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // wynik
+            // 
+            this.wynik.Location = new System.Drawing.Point(228, 184);
+            this.wynik.Name = "wynik";
+            this.wynik.Size = new System.Drawing.Size(148, 20);
+            this.wynik.TabIndex = 6;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.progressBar1.Location = new System.Drawing.Point(228, 165);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(148, 13);
+            this.progressBar1.Step = 20;
+            this.progressBar1.TabIndex = 7;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(518, 233);
+            this.button7.Name = "button7";
+            this.button7.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "RESET";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // timer6
+            // 
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(71, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(451, 31);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Zbadaj autentyczność dokumentu";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // TesterAutentycznosci
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(605, 268);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.wynik);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TesterAutentycznosci";
+            this.Text = "TesterAutentycznosci";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox wynik;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SprawdzDokument/TesterAutentycznosci.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SprawdzDokument
+{
+    public partial class TesterAutentycznosci : Form
+    {
+        int x;
+        Random random = new Random();
+        public TesterAutentycznosci()
+        {
+            InitializeComponent();
+        }
+    
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x = random.Next(0, 100);
+            this.timer1.Start();  
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            x = random.Next(0, 100);
+            this.timer2.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            x = random.Next(0, 100);
+            this.timer3.Start();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            x = random.Next(0, 100);
+            this.timer4.Start();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            x = random.Next(0, 100);
+            this.timer5.Start();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            x = random.Next(0, 100);
+            this.timer6.Start();
+        }
+
+
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.timer1.Stop();
+            this.timer2.Stop();
+            this.timer3.Stop();
+            this.timer4.Stop();
+            this.timer5.Stop();
+            this.timer6.Stop();
+            wynik.Text = "";
+            progressBar1.Value = 0;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.progressBar1.Increment(1);
+            if (progressBar1.Value == progressBar1.Maximum)
+            {
+                this.timer1.Stop();
+                if (x<=60) { wynik.Text = "Dokument AUTENTYCZNY"; } else { wynik.Text = "Dokument PODROBIONY"; }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            this.progressBar1.Increment(1);
+            if (progressBar1.Value == progressBar1.Maximum)
+            {
+                this.timer1.Stop();
+                if (x <= 50) { wynik.Text = "Dokument AUTENTYCZNY"; } else { wynik.Text = "Dokument PODROBIONY"; }
+            }
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            this.progressBar1.Increment(1);
+            if (progressBar1.Value == progressBar1.Maximum)
+            {
+                this.timer1.Stop();
+                if (x <= 70) { wynik.Text = "Dokument AUTENTYCZNY"; } else { wynik.Text = "Dokument PODROBIONY"; }
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            this.progressBar1.Increment(1);
+            if (progressBar1.Value == progressBar1.Maximum)
+            {
+                this.timer1.Stop();
+                if (x <= 80) { wynik.Text = "Dokument AUTENTYCZNY"; } else { wynik.Text = "Dokument PODROBIONY"; }
+            }
+
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            this.progressBar1.Increment(1);
+            if (progressBar1.Value == progressBar1.Maximum)
+            {
+                this.timer1.Stop();
+                if (x <= 90) { wynik.Text = "Dokument AUTENTYCZNY"; } else { wynik.Text = "Dokument PODROBIONY"; }
+            }
+        }
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            this.progressBar1.Increment(1);
+            if (progressBar1.Value == progressBar1.Maximum)
+            {
+                this.timer1.Stop();
+                if (x <= 95) { wynik.Text = "Dokument AUTENTYCZNY"; } else { wynik.Text = "Dokument PODROBIONY"; }
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
